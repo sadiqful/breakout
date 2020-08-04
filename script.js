6a565f67e740361aa0902d2d52867d5321a7933c
Aliyu Abubakar <sadiqful@gmail.com>
2020-08-04 18:55:02 +0100

    ball and elements

---
+++ script.js
@@ -0,0 +1,20 @@
+const rulesBtn = document.getElementById('rules-btn')
+const closeBtn = document.getElementById('close-btn')
+const rules = document.getElementById('rules')
+const canvas = document.getElementById('canvas')
+const ctx = canvas.getContext('2d');
+
+const brickRowCount = 9;
+const brickColumnCount = 5;
+const delayReset = 500;
+
+const ball = {
+    x: canvas.width / 2,
+    y: canvas.hight / 2,
+    size: 10,
+    speed: 4,
+    dx: 4,
+    dy: -4,
+    visible: true
+};
+
