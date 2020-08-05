c255cfa635d003c767bcd6bc2af11fa8ad0bb6d9
Aliyu Abubakar <sadiqful@gmail.com>
2020-08-05 10:30:56 +0100

    drawing ball

--- Js/script.js
+++ Js/script.js
@@ -49,3 +49,12 @@ for (let i = 0; i < brickColumnCount; i++) {
     }
 }
 
+// drawing balls 
+
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(ball.x, ball.y, ball.size, 0, Math.PI * 2);
+    ctx.fillStyle = ball.visible ? '#0095dd' : 'transparent';
+    ctx.fill();
+    ctx.clothPath();
+}
