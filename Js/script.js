a4877a9019adba38ebe86115622108af7af2818a
Aliyu Abubakar <sadiqful@gmail.com>
2020-10-17 16:09:25 +0100

    typo corrected

--- Js/script.js
+++ Js/script.js
@@ -56,7 +56,7 @@ function drawBall() {
   ctx.arc(ball.x, ball.y, ball.size, 0, Math.PI * 2);
   ctx.fillStyle = ball.visible ? "#0095dd" : "transparent";
   ctx.fill();
-  ctx.clothPath();
+  ctx.closePath();
 }
 
 // drawong paddle
